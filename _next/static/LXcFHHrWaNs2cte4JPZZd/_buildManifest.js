544f5bd0afe6d74faae462cf15ba5d19ab9db90b
totagits <tis@totaggroup.com>
2026-09-05 14:50:54 -0500

    deploy: complete static site with /LACD-Platform base path and assets

---
+++ _next/static/LXcFHHrWaNs2cte4JPZZd/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/LACD-Platform//_next/:path+",
+        "destination": "/LACD-Platform/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
